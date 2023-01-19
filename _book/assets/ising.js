4273138b6eeab124b91e2691ba4ceb17182bdd0a
Austen Lamacraft <austen.lamacraft@gmail.com>
2023-01-19 10:42:50 +0000

    More monte carlo

--- _book/assets/ising.js
+++ _book/assets/ising.js
@@ -13,46 +13,77 @@ async function loadSim() {
     return pyodide.pyimport("ising")
   }
 
-let ising_model // will hold the simulation
+ising = function(p) {
+  let isingModel // will hold the simulation
 
-const w = 5; // Size of each site
-let started = false
+  const w = 5; // Size of each site
+  let started = false
+  let update = "Glauber"
+  let sublattice = true
 
-async function setup() {
-    const ising = await loadSim();
-    started = true
-    noLoop()
-    const canvas = createCanvas(400, 400);
-    canvas.parent('ising-simulation');
-
-    const L = floor(width/w);
-    ising_model = ising.IsingModel(L) 
-
-    button = createButton('\u23F8');
-    button.parent('ising-simulation')
-    button.position(-width, -12, 'relative')
-    button.mousePressed(() => isLooping() ? noLoop() : loop());
+  p.setup = async function() {
+      const ising = await loadSim();
+      started = true
+      p.noLoop()
+      p.createCanvas(400, 400);
+      // p.createCanvas(p.windowWidth / 1.5, p.windowHeight / 2);
     
-    slider = createSlider(1, 3.5, 2.269, 0.01);
-    slider.parent('ising-simulation')
-    slider.style('width', '150px');
-    slider.position(0, 20, 'relative').center('horizontal');
-}
+      const L = p.floor(p.width / w);
+      isingModel = ising.IsingModel(L) 
 
-function draw() {
-  background(255);
-  if (started) {  
-    ising_model.glauber_update(1 / slider.value())
-    config = ising_model.to_js()
-  
-    config.forEach((col, colIdx) => {
-      col.forEach((site, rowIdx) => {
-        if (site) fill(0);
-        else fill(255);
-        stroke(0);
-        rect(colIdx * w, rowIdx * w, w, w);
+      button = p.createButton('\u23EF');
+      button.parent('ising-simulation')
+      button.position(0, p.height - 40, 'absolute')
+      button.mousePressed(() => p.isLooping() ? p.noLoop() : p.loop());
+      
+      slider = p.createSlider(1, 3.5, 2.269, 0.01);
+      slider.parent('ising-simulation')
+      slider.style('width', '150px');
+      slider.position(0, p.height, 'absolute');
+      
+      const updateSelector = p.createSelect()
+        .style('font-size', '16px')
+        .parent("ising-simulation")
+        .position(0, 0, 'absolute')
+        .size(100)
+      
+      updateSelector.option('Glauber')
+      updateSelector.option('Block Gibbs')
+      updateSelector.option('Wolff')
+      
+      const setUpdate = function() {
+        update = updateSelector.value();
+        console.log(update)
+      }
+      
+      updateSelector.changed(setUpdate);
+  }
+
+  p.draw = function() {
+    p.background(255);
+    if (started) {  
+      if (update == "Glauber") {
+        isingModel.glauber_update(1 / slider.value())
+      }
+      else if (update === "Block Gibbs") {
+        isingModel.gibbs_update(1 / slider.value(), sublattice)
+        sublattice = !sublattice  
+      }
+      else {
+        isingModel.wolff_update(1 / slider.value())  
+      }
+      spins = isingModel.to_js()
+    
+      spins.forEach((col, colIdx) => {
+        col.forEach((site, rowIdx) => {
+          if (site == 1) p.fill(0);
+          else p.fill(255);
+          p.stroke(0);
+          p.rect(colIdx * w, rowIdx * w, w, w);
+        })
       })
-    })
+    }
   }
 }
 
+new p5(ising, "ising-simulation")
--- assets/ising.py
+++ assets/ising.py
@@ -2,24 +2,21 @@ import numpy as np
 from pyodide.ffi import to_js
 from collections import deque 
 
-
 class IsingModel:
     def __init__(self, L):
         self.L = L
         self.spins = np.random.choice(a=[1, -1], size=(L, L))
+        stagger = np.empty(self.L, dtype = bool)
+        stagger[::2] = True
+        stagger[1::2] = False
+        self.mask = np.logical_xor(stagger[:, np.newaxis], stagger[np.newaxis, :])
 
-    def gibbs_update(self, beta):
-        '''
-        Choose roughly half the spins to update at random.
-        Strictly this is not correct as adjacent spins are not independent
-        but it makes a nice picture.
-        '''
+    def gibbs_update(self, beta, sublattice):
         fields = np.roll(self.spins, 1, 0) + np.roll(self.spins, -1, 0) + np.roll(self.spins, 1, 1) + np.roll(self.spins, -1, 1)
         delta_E = 2 * fields
         spin_up_probabilities = 1 / (1 + np.exp(- beta * delta_E))
-        new_spins = 2 * (np.random.random_sample(size=(self.L, self.L)) < spin_up_probabilities) - 1
-        to_update = np.random.choice(a=[True, False], size=(self.L, self.L))
-        self.spins = np.choose(to_update, [self.spins, new_spins])
+        new_spins = 2 * (np.random.rand(self.L, self.L) < spin_up_probabilities) - 1
+        self.spins = np.choose(np.logical_xor(sublattice, self.mask), [self.spins, new_spins])
 
     def glauber_update(self, beta):
         x, y = np.random.randint(self.L, size=2)
@@ -34,9 +31,6 @@ class IsingModel:
             self.spins[x, y] = -1
 
     def wolff_update(self, beta):
-        '''
-        Wolff cluster update
-        '''
         initial_x, initial_y = np.random.randint(self.L, size=2)
         initial_spin = self.spins[initial_x, initial_y]
         cluster = deque([(initial_x, initial_y)])
